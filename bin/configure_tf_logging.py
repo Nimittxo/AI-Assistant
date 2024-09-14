ec55d10575260ae167ee78c1d98ea64bba2166d1
Nimittxo <akki042005@gmail.com>
2024-09-15 03:24:59 +0530

    TF_log_file

--- bin/configure_tf_logging.py
+++ bin/configure_tf_logging.py
@@ -3,4 +3,7 @@ import tensorflow as tf
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Setting the @LOG_LEVEL to 3 for extreme suppressions!
 
+#setting up the TensorFlow warnings to redirect and write up to a file: 
+TF_log_file = 'tensorflow_logs.txt'
+
 print("Chnaging the configuration of TF!")
